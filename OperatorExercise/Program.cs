e9ca4ca8bb59c1613f671c77b282ee33b9a32377
dylan wuu <dylanwuu@oulook.com>
2023-06-22 14:34:07 -0700

    exercise completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,32 @@
 {
     public class Program
     {
+        public static double CircleArea(double r)
+        {
+            double area = Math.PI * Math.Pow(r, 2);
+            return area;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("second number");
+            int b = int.Parse(Console.ReadLine());
+            int add = a + b;
+            int sub = a - b;
+            int multi = a * b;
+            int div = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} + {b} = {add}");
+            Console.WriteLine($"{a} - {b} = {sub}");
+            Console.WriteLine($"{a} * {b} = {multi}");
+            Console.WriteLine($"{a} / {b} = {div}, with a remainder of {remainder}");
+
+            Console.WriteLine("what is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var cArea = CircleArea(radius);
+            Console.WriteLine($"the area of a circle with a radius of {radius} is {cArea}");
         }
     }
 }
